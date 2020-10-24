c1c8f5b54ac90f545de09848766afcadfd784d8c
gabopereyra <gabrielp1689@gmail.com>
2020-10-23 23:13:09 -0300

    Se genera el código para los archivos controladores. Se divide el proyecto en modulos.

--- controladores/rutasC.php
+++ controladores/rutasC.php
@@ -1,10 +1,21 @@
 <?php
 
 class RutasControlador{
-public function Plantilla(){
-    include "vistas/plantillas.php";
+    public function Plantilla(){
+        include 'vistas/plantilla.php';
     }
 
+    public function Rutas(){
+        if(isset($_GET["ruta"])){
+            $rutas = $_GET["ruta"];
+        } else{
+            $rutas = "index";
+        }
 
+        $respuesta = Modelo::RutasModelo($rutas);
+
+        include $respuesta;
+    }
 
 }
+
---
+++ vistas/modulos/salir.php
@@ -0,0 +1,8 @@
+<section>
+
+	<br><br><br><hr>
+	<h1>Has cerrado sesión</h1>
+	<hr>
+
+</section>
+	
