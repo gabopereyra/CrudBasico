4bebd6caaf6d190cbca2a105f52fe70da3c5a4ea
gabopereyra <gabrielp1689@gmail.com>
2020-10-26 20:08:11 -0300

    Se genera modelo y controlador para cargar de empleados

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 
 include_once 'controladores/rutasC.php';
 include_once 'controladores/adminC.php';
+include_once 'controladores/empleadosC.php';
+
 include_once 'modelos/rutasM.php';
 include_once 'modelos/adminM.php';
+include_once 'modelos/empleadosM.php';
 
 $rutas=new RutasControlador();
 $rutas->Plantilla();
